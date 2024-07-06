36ce571b29b2dd6897b14d6a0f8f0103dff235e8
jfrlysona <jfrlysona@gmail.com>
2024-07-06 12:49:52 +0400

    added timestamps

--- src/model/OrderModel.js
+++ src/model/OrderModel.js
@@ -1,26 +1,34 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const ordersSchema = new Schema({
-  amount: { type: Number, required: true },
-  status: { type: String, required: true, default: "pending" },
-  promocode: [
-    {
-      code: { type: String, required: true },
-      discount: { type: Number, required: true },
-      expirationDate: { type: Date, required: true },
-      limit: { type: Number, default: 0, ref: "users" },
+const ordersSchema = new Schema(
+  {
+    amount: { type: Number, required: true },
+    status: { type: String, required: true, default: "pending" },
+    promocode: [
+      {
+        code: { type: String, required: true },
+        discount: { type: Number, required: true },
+        expirationDate: { type: Date, required: true },
+        limit: { type: Number, default: 0, ref: "users" },
+      },
+    ],
+    paymentMethod: { type: String, required: true },
+    items: [
+      {
+        itemId: { type: Schema.Types.ObjectId, ref: "items" },
+        itemCount: { type: Number, required: true },
+      },
+    ],
+    orderByUserId: { type: Schema.Types.ObjectId, ref: "users" },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: false,
     },
-  ],
-  paymentMethod: { type: String, required: true },
-  items: [
-    {
-      itemId: { type: Schema.Types.ObjectId, ref: "items" },
-      itemCount: { type: Number, required: true },
-    },
-  ],
-  orderByUserId: { type: Schema.Types.ObjectId, ref: "users" },
-});
+  }
+);
 
 const OrdersModel = mongoose.model("orders", ordersSchema);
 module.exports = OrdersModel;
